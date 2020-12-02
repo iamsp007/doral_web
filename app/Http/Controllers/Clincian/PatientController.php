1f8c373a913206e06bcbec1aacaf3504c737b5ba
Sunil Karmur <sunil.k@lipitech.in>
2020-12-02 22:53:39 +0530

    change patient query

--- app/Http/Controllers/Clincian/PatientController.php
+++ app/Http/Controllers/Clincian/PatientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Clincian;
 
 use App\Http\Controllers\Controller;
+use App\Models\PatientReferral;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
@@ -23,17 +24,9 @@ class PatientController extends Controller
 
     public function getPatientList(){
 
-        $patientList = User::where('type','=','Patient')->get();
+        $patientList = PatientReferral::with('detail')->get();
         return DataTables::of($patientList)
             ->addIndexColumn()
-            ->addColumn('action', function($row){
-
-                $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct">Accept</a>';
-
-                $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct">Reject</a>';
-
-                return $btn;
-            })
             ->rawColumns(['action'])
             ->make(true);
     }

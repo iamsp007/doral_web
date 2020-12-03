688165b180e68ba8f7369988eba4d89f07426918
mayank Abiyani <mayank@ainfosolutions.com>
2020-12-03 19:08:35 +0530

    URL Issues in Referral Sides

--- resources/views/includes/referral/left.blade.php
+++ resources/views/includes/referral/left.blade.php
@@ -1,17 +1,24 @@
 <div class="block">
-                    <!-- Logo Start -->
-    <a href="/referral/dashboard" title="Welcome to Doral">
-        <img src="../assets/img/logo-white.svg" alt="Welcome to Doral"
-            srcset="../assets/img/logo-white.svg" class="img-fluid">
+    <!-- Logo Start -->
+    <a href="{{ route('referral.dashboard') }}" title="Welcome to Doral">
+        <img src="{{asset('assets/img/logo-white.svg')}}" alt="Welcome to Doral"
+            srcset="{{asset('assets/img/logo-white.svg')}}" class="img-fluid">
     </a>
     <!-- Logo End -->
     <i class="las la-times-circle la-3x white d-block d-xl-none d-lg-none d-md-none d-sm-none"
         id="closeMenu"></i>
 </div>
 <ul class="sidenav">
-    <li><a <?php if(request()->segment(count(request()->segments())) == 'dashboard') {?> class="active" <?php } ?>href="/referral/dashboard">Dashboard<span class="dot"></span></a></li>
-    <li><a <?php if(request()->segment(count(request()->segments())) == 'vbc') {?> class="active" <?php } ?>href="/referral/vbc">VBC<span class="dot"></span></a></li>
-    <li><a <?php if(request()->segment(count(request()->segments())) == 'md-order') {?> class="active" <?php } ?>href="/referral/md-order">MD Order<span class="dot"></span></a></li>
-    <li><a <?php if(request()->segment(count(request()->segments())) == 'employee-pre-physical' || request()->segment(count(request()->segments())) == 'employee-pre-physical-upload-bulk-data') {?> class="active" <?php } ?> href="/referral/employee-pre-physical">Employee Pre-Physical<span class="dot"></span></a></li>
-    <li><a href="/referral/logout">Log Out<span class="dot"></span></a></li>
+    <li><a class="{{ \Request::is('referral/dashboard')?'active':'' }}" href="{{ route('referral.dashboard') }}">Dashboard <span class="dot"></span></a></li>
+    <li><a class="{{ \Request::is('referral/vbc')?'active':'' }}" href="{{ route('referral.vbc') }}">VBC <span class="dot"></span></a></li>
+    <li><a class="{{ \Request::is('referral/md-order')?'active':'' }}" href="{{ route('referral.md-order') }}">MD Order <span class="dot"></span></a></li>
+    <li><a class="{{ \Request::is('referral/employee-pre-physical')?'active':'' }}" href="{{ route('referral.employee-pre-physical') }}">Employee Pre-Physical<span class="dot"></span></a></li>
+    <li>
+        <a href="{{ url('logout') }}"
+           onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+        >Logout <span class="dot"></span></a>
+        <form id="logout-form" action="{{ url('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </li>
 </ul>
--- resources/views/pages/referral/md-order-upload-bulk-data.blade.php
+++ resources/views/pages/referral/md-order-upload-bulk-data.blade.php
@@ -69,9 +69,11 @@
                     <tr>
                         <th><input type="checkbox" class="selectall" /></th>
                         <th>Patient Name</th>
-                        <th>Description</th>
-                        <th>Services</th>
-                        <th>Uploaded Date</th>
+                        <th>Gender</th>
+                        <th>Phone</th>
+                        <th>City</th>
+                        <th>Zip Code</th>
+                        <th>Created Date</th>
                         <th>Status</th>
                         <th></th>
                     </tr>
@@ -82,12 +84,14 @@
                     <tr>
                         <td><input type="checkbox" /></td>
                         <td class="text-green">{{$raw['first_name']}} {{$raw['middle_name']}} {{$raw['last_name']}}</td>
-                        <td width="20%">Curabitur dignissim tortor.</td>
-                        <td>Employee Pre Physical</td>
+                        <td>{{$raw['gender']}}</td>
+                        <td>{{$raw['phone1']}}</td>
+                        <td>{{$raw['city']}}-{{$raw['state']}}</td>
+                        <td>{{$raw['Zip']}}</td>
                         <td>{{ date('F d Y', strtotime($raw['created_at'])) }} <!--Sunday, 4 October 2020--></td>
                         <td class="text-green">Success</span></td>
                         <td width="9%"><a href="javascript:void(0)"><img
-                                    src="../assets/img/icons/delete-icon.svg"
+                                    src="{{asset('assets/img/icons/delete-icon.svg')}}"
                                     class="action-delete" /></a>
                         </td>
                     </tr>

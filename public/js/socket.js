db56346ed655ba48b3227da7e733b4436f8e4338
Sunil Karmur <sunil.k@lipitech.in>
2020-12-09 19:25:19 +0530

    issue fix

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,8 +1,5 @@
-var socket = io('ws://localhost:3001', {
+var Socket = io('ws://localhost:3001', {
     token: 1,
     transports: ['websocket']
 });
-socket.on('connect', function () {
-    console.log('send-location!');
-    socket.emit('send-location', { message: 'Hello Mr.Server!' });
-});
+

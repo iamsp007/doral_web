492c194f6307b6a503ab46e51ae7292cc7c2d227
KalpeshAIS <kalpesh@ainfosolutions.com>
2020-12-26 02:21:35 +0530

    changes for calander

--- routes/coordinator.php
+++ routes/coordinator.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+
 Route::group(['prefix'=>'/co-ordinator','middleware'=>['auth','role:co-ordinator']],function (){
     Route::get('/','\App\Http\Controllers\Coordinator\CoordinatorController@index')->name('coordinator.dashboard');
     Route::get('/patient-list-show','\App\Http\Controllers\Coordinator\CoordinatorController@patientListShow')->name('coordinator.patientListShow');
@@ -9,6 +10,21 @@ Route::group(['prefix'=>'/co-ordinator','middleware'=>['auth','role:co-ordinator
     Route::get('/new-patient-list-show','\App\Http\Controllers\Coordinator\CoordinatorController@newPatientListShow')->name('coordinator.newPatientListShow');
     Route::get('/get-new-patient-list','\App\Http\Controllers\Coordinator\CoordinatorController@getNewPatientList')->name('coordinator.getNewPatientList');
 
-    Route::get('/appointment/{pid}','\App\Http\Controllers\Coordinator\AppointmentController@index')->name('coordinator.appointment');
     
+
+    Route::group(['prefix'=>'/appointment'],function (){
+    	
+    	Route::get('/create','\App\Http\Controllers\Coordinator\AppointmentController@create')->name('coordinator.appointment.create');
+
+        Route::post('/store', 'App\Http\Controllers\Coordinator\AppointmentController@store')->name('coordinator.appointment.store');
+
+	    Route::get('/{pid}','\App\Http\Controllers\Coordinator\AppointmentController@index')->name('coordinator.appointment');
+	    
+	    
+   }); 
+
+
+
+    
+
 });

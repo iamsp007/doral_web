5f8279e56b3cee517e5bb2865acfed6ecfb9494c
Sunil Karmur <sunil.k@lipitech.in>
2021-01-05 00:46:29 +0530

    calender api call

--- resources/views/pages/coordinator/appointment.blade.php
+++ resources/views/pages/coordinator/appointment.blade.php
@@ -7,68 +7,154 @@
 @section('content')
     <div id="calendar"></div>
 @endsection
-@section('popup')
-    <div id="book_app_popup"></div>
-@endsection
 @push('styles')
     <link rel="stylesheet" href="{{ asset('assets/css/daterangepicker.min.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/calendar/lib/main.css') }}">
+    <style>
+        #calendar {
+            max-width: 1100px;
+            margin: 0 auto;
+        }
+    </style>
 @endpush
 
 @push('scripts')
-    <script src="{{ asset('assets/js/daterangepicker.min.js') }}"></script>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <script src="{{ asset('assets/calendar/lib/main.js') }}"></script>
     <script>
-        $(document).ready(function() {
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
 
-
-            $('#calendar').fullCalendar({
-                defaultView: 'agendaDay',
-                selectable: true,
-                defaultTimedEventDuration: '00:15:00',
-                minTime: "08:00:00",
-                maxTime: "23:00:00",
-                slotDuration: '00:30:00',
-                slotLabelInterval: 15,
-                slotLabelFormat: 'h(:mm)a',
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'agendaDay,agendaWeek,agendaMonth'
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                timeZone: 'IST',
+                initialView: 'dayGridMonth',
+                height: '100%',
+                themeSystem: 'bootstrap4',
+                aspectRatio: 2,
+                windowResizeDelay: 100,
+                stickyHeaderDates: true,
+                headerToolbar: {
+                    left: 'title',
+                    center: '',
+                    right: 'dayGridMonth timeGridDay today prev,next'
                 },
-                businessHours: [{
-                    dow: [1, 2, 3, 4, 5], // Monday - Friday
-                    start: '08:00',
-                    end: '12:00',
-                }, {
-                    dow: [1, 2, 3, 4, 5], // Monday - Friday (if adding lunch hours)
-                    start: '13:00',
-                    end: '17:00',
-                }],
-                selectConstraint: "businessHours",
-                select: function(start, end, jsEvent, view) {
-                    if (start.isAfter(moment())) {
+                expandRows: true,
+                navLinks: true, // can click day/week names to navigate views
+                selectable: true,
+                selectMirror: true,
+                selectHelper: true,
+                businessHours: false,
+                eventColor: '#008591',
+                select: function(arg) {
+                    if(moment(arg.start).isBefore(moment())) {
+                        calendar.unselect();
+                        return false;
+                    }
 
-                        var eventTitle = prompt("Provide Event Title");
-                        if (eventTitle) {
-                            $("#calendar").fullCalendar('renderEvent', {
-                                title: eventTitle,
-                                start: start,
-                                end: end,
-                                stick: true
-                            });
-                            alert('Appointment booked at: ' + start.format("h(:mm)a"));
+                    if (arg.view.type==="timeGridDay"){
+
+                        return false;
+                    }
+
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url:'{{ route("coordinator.calender.timeslot") }}',
+                        method:'POST',
+                        data:{
+                            date:moment(arg.start).format('YYYY-MM-DD')
+                        },
+                        dataType:'json',
+                        success:function (response) {
+                            var events=[];
+                            if (response.status===true){
+                                if (response.data.length>0){
+                                    response.data.map(function (value) {
+                                        events.push({
+                                            id:value.id,
+                                            start: moment(value.time).format('YYYY-MM-DD HH:mm'),
+                                            end:moment(value.time).add(30,'minutes').format('YYYY-MM-DD HH:mm'),
+                                            backgroundColor:'#FFFFFF',
+                                            allDay: false
+                                        })
+                                    })
+                                    calendar.removeAllEvents();
+                                    calendar.addEventSource(events)
+                                    calendar.changeView('timeGridDay',moment(arg.start).format('YYYY-MM-DD'))
+                                }else {
+                                    alert("This Date Clinician Not Available")
+                                }
+                            }
+                        },
+                        error:function (error) {
+
                         }
-                    } else {
-                        alert('Cannot book an appointment in the past');
+                    })
+                    calendar.unselect()
+
+                },
+                eventClick: function(info) {
+                    var eventObj = info.event;
+                    var date = moment(info.event.startStr).format('YYYY-MM-DD HH:mm');
+                    if (eventObj.id){
+                        createAppointment(date,eventObj.id);
+                    }
+
+                    // $('.dialogue').modal('show');
+                },
+                eventDidMount: function (info) {
+                  //  console.log(info.el)
+                },
+
+                editable: true,
+                dayMaxEvents: true, // allow "more" link when too many events
+                events: [],
+                buttonText: {
+                    today: 'Today',
+                    month: 'Month',
+                    week: 'Week',
+                    day: 'Day',
+                    list: 'List'
+                },
+                views: {
+                    dayGridMonth: { // name of view
+                        titleFormat: { year: 'numeric', month: 'short', day: 'numeric' }
+                        // other view-specific options here
+                    },
+                    timeGridFourDay: {
+                        type: 'timeGrid',
+                        duration: { days: 6 },
+                        buttonText: '4 day'
                     }
                 },
-                eventClick: function(calEvent, jsEvent, view) {
-                    alert('Event: ' + calEvent.title);
-                }
             });
+
+            calendar.render();
         });
+
+        function createAppointment(date,ids) {
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url:'{{ route("coordinator.appointment.store") }}',
+                method:'POST',
+                data:{
+                    book_datetime:date,
+                    patient_id:'{{ $patientId }}',
+                    clinician_ids:ids
+                },
+                dataType:'json',
+                success:function (response) {
+                    alert(response.message)
+                },
+                error:function (request, status, error) {
+                    const sources = JSON.parse(request.responseText);
+                    alert(sources.message)
+                }
+            })
+        }
+
     </script>
     @endpush

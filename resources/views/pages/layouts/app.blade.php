23ba70cd6a4fcb718a52fd2a6ae75fde320900e9
hcbstech <shashikant@hcbspro.com>
2021-01-04 16:19:52 +0530

    login

--- resources/views/pages/layouts/app.blade.php
+++ resources/views/pages/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/fonts/Montserrat.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/line-awesome.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('assets/css/tail.select-default.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/sidebar.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">

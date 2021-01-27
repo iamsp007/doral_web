39f1149b6b2b04fe06f988f0726c9cf9c0c0f13b
gauravhcbs <gauravpatel@hcbc.com>
2021-01-27 20:02:02 +1300

    supervisor

--- routes/supervisor.php
+++ routes/supervisor.php
@@ -3,8 +3,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'/supervisor','middleware'=>['auth','role:supervisor']],function (){
     Route::get('/','\App\Http\Controllers\Supervisor\SuperVisorController@index')->name('supervisor.dashboard');
-    Route::get('/patients','\App\Http\Controllers\Supervisor\SuperVisorController@viewPatients')->name('supervisor.patients');
+
+    Route::get('/patients','\App\Http\Controllers\Supervisor\SuperVisorController@viewNewPatients')->name('supervisor.patients');
     Route::get('/patient-list','\App\Http\Controllers\Supervisor\SuperVisorController@getPatients')->name('supervisor.patients.ajax');
-    Route::post('/case_management','\App\Http\Controllers\Supervisor\SuperVisorController@case_management');
+    Route::post('/add-case-management','\App\Http\Controllers\Supervisor\SuperVisorController@add_case_management');
+
+    Route::get('/assigned-patients','\App\Http\Controllers\Supervisor\SuperVisorController@viewAssigedPatients')->name('supervisor.assigned_patients.ajax');
+    Route::get('/assigned-patient-list','\App\Http\Controllers\Supervisor\SuperVisorController@getAssignedPatients')->name('supervisor.assignedpatients.ajax');
+    Route::post('/update-case-management','\App\Http\Controllers\Supervisor\SuperVisorController@update_case_management');
 
 });

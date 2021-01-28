3f43689752341855d7b06f35a66b07828f57d81e
sunil-k <sunil.ahir@globalgarner.com>
2021-01-28 18:10:46 +0530

    referral data upload

---
+++ app/Models/SymptomsMaster.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SymptomsMaster extends Model
+{
+    use HasFactory;
+
+    public function getImgAttribute($value)
+    {
+        if ($value) {
+            return asset('images/' . $value);
+        } else {
+            return asset('images/no-image.png');
+        }
+    }
+}

a245616ad2e7b3bd348356265ca85b3705a84ada
sunil-k <sunil.ahir@globalgarner.com>
2021-02-03 20:36:47 +0530

    firebase key change

--- public/js/firebase-messaging-sw.js
+++ public/js/firebase-messaging-sw.js
@@ -10,10 +10,10 @@ Initialize the Firebase app in the service worker by passing in the messagingSen
 * New configuration for app@pulseservice.com
 */
 firebase.initializeApp({
-    apiKey: "AIzaSyCVKDvGuHvojFULepdxiU4h1I5mzM4Rxoc",
-    projectId: "laravel-2732a",
-    messagingSenderId: "105532575378",
-    appId: "1:105532575378:web:caa2aa50e10a09299de04b"
+    apiKey: "AIzaSyC5rTr8rSUyQeKlbaAHW1Xo-ezNoQO0dUE",
+    projectId: "doral-roadl",
+    messagingSenderId: "606071434218",
+    appId: "1:606071434218:web:8ba9b96b1af8ff8309a093"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background

3f940df9d23b7a87f78eb4c173b39307016fdcd9
Ubuntu <ubuntu@ip-172-31-36-227.us-east-2.compute.internal>
2021-02-05 15:09:08 +0000

    pass

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -114,6 +114,24 @@
             }
             console.log(type)
         }
+        $(".toggle-password").click(function() {
+        $('.pass-show').click(function (event) {
+            $(".pass-hide").addClass('d-block').removeClass('d-none');
+            $(".pass-show").addClass('d-none').removeClass('d-block');
+            
+        });
+        $('.pass-hide').click(function (event) {
+            $(".pass-hide").addClass('d-none').removeClass('d-block');
+            $(".pass-show").addClass('d-block').removeClass('d-none');
+            
+        });
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+      });
 
     </script>
 @endpush

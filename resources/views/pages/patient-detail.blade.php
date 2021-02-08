d3273b77c0bd6a6582b2d4a1f0695140cb91eaa3
manishak <manishak@hcbspro.com>
2021-02-08 11:12:32 +0530

    Added:dynamicallly dropdown

--- resources/views/pages/patient-detail.blade.php
+++ resources/views/pages/patient-detail.blade.php
@@ -2208,14 +2208,13 @@
                                 }
                                
                                 html +='"><th scope="row">' + data.count+ '</th><td scope="row">' + data.result.lab_report_type.name +'</td><td>' + data.result.due_date + '</td><td>' + data.result.expiry_date + '</td><td>' + data.result.lab_result + '</td><td class="text-center"><span onclick="exploder(tb1)" id="tb1" class="exploder"><i class="las la-plus la-2x"></i></span><a href="javascript:void(0)" class="deleteLabResult" data-id="1"><i class="las la-trash la-2x text-white pl-4"></i></a></td></tr>';
-                            
-                                $('.list-order tr:last').before(html);
+                               
+                                $('.tb-list-order tr:last').before(html);
                               
+                                $(document).find('.sequence').text(data.newCount);
+
                                 var select = $('#lab_report_type_id').empty();
                                 select.append('<option value="">Select a test type</option>');
-
-                               
-                                $(document).find('.sequence').text(data.newCount);
 
                                 $.each(data.tbLabReportTypes, function (key, value) {
                                     select.append('<option value="' + value.id + '">' + value.name + '</option>');
@@ -2234,7 +2233,8 @@
             $('body').on('click', '.deleteLabResult', function () {
                 var t = $(this);
                 var id = t.attr("id");
-
+                var patient_referral_id = $(this).data("id") ;
+               
                 swal({
                     title: "Are you sure?",
                     text: "Are you sure want to delete this record?",
@@ -2251,6 +2251,7 @@
                             },
                             data: {
                                 "id": id,
+                                "patient_referral_id" : patient_referral_id
                             },
                             'success': function (data) {
                                 if(data.status == 400) {
@@ -2262,6 +2263,15 @@
                                 } else {
                                     t.parents("tr").fadeOut(function () {
                                         $(this).remove();
+                                    });
+
+                                    $(document).find('.sequence').text(data.newCount);
+
+                                    var select = $('#lab_report_type_id').empty();
+                                    select.append('<option value="">Select a test type</option>');
+                                    alert(data.tbLabReportTypes);
+                                    $.each(data.tbLabReportTypes, function (key, value) {
+                                        select.append('<option value="' + value.id + '">' + value.name + '</option>');
                                     });
                                     
                                     swal(

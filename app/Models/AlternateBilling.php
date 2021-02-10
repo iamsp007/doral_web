c1b58fa50d82787dd1f5bd4a309fd18e131e8be4
manishak <manishak@hcbspro.com>
2021-02-10 18:55:30 +0530

    Added:models

---
+++ app/Models/AlternateBilling.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AlternateBilling extends Model
+{
+    use HasFactory;
+}

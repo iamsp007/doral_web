9c9d20f6b3f9ec04fd7dbcee34117b3cc7688d9c
manishak <manishak@hcbspro.com>
2021-02-20 14:19:15 +0530

    Added:search cargiver

---
+++ resources/views/pages/patient_detail/ccm.blade.php
@@ -0,0 +1,9 @@
+<div class="tab-pane fade" id="ccm" role="tabpanel"
+        aria-labelledby="ccm-tab">
+    <div class="app-card app-card-custom" data-name="clinical" >
+        <div class="app-card-header">
+            <h1 class="title">CCM</h1>
+        </div>
+        @include('includes.calendar.ccm.script')
+    </div>
+</div>

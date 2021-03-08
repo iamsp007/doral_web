6d57ec6e23b6310349d21f53370cc83b6733c296
Shashikant Parmar <shashikant@hcbspro.com>
2021-03-08 18:25:22 +0530

    form

---
+++ resources/views/pages/modals/labreports.blade.php
@@ -0,0 +1,208 @@
+<div class="modal fade" id="labreportModal" tabindex="-1" aria-labelledby="labreportModal" aria-hidden="true">
+    <div class="modal-dialog modal-large modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">View Lab Reports</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="pb-5">
+                    <div class="d-flex justify-content-end mb-4">
+                        <button type="submit" class="btn btn-outline-green d-flex align-items-center"
+                                name="Download">Download All Reports</button>
+                    </div>
+                    <div class="scrollbar scrollbar9" id="view-lab-report-file">
+                        <div class="row">
+                            <div class="col-12 col-sm-2 mt-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" onclick="openDoc()"
+                                           class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-pdf la-2x"></i> All Bill.pdf
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" data-toggle="modal" data-target="#docViewerModal"
+                                                       href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-2 mt-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-pdf la-2x"></i> All Bill.pdf
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-2 mt-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-word la-2x"></i> Healthcare.docx
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-2">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-word la-2x"></i> Lab.docx
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-2 mt-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-word la-2x"></i> X-Ray.docx
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-2 mt-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-word la-2x"></i> CHHA.docx
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-2 mt-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <img class="img-fluid" alt="" jsaction="load:G7tQM" data-drive-wiz-load-handling=""
+                                             src="../assets/img/All Banner copy.docx.png">
+                                    </div>
+                                    <div class="card-footer file-footer">
+                                        <a href="javascript:void(0)" class="d-flex align-items-center text-success">
+                                            <i class="lar la-file-word la-2x"></i> CHHA.docx
+                                        </a>
+                                        <div>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="las la-ellipsis-v"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Download File</a>
+                                                    <a class="dropdown-item" href="#">View Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

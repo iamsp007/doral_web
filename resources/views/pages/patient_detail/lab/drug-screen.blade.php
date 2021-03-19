a1a60af80afe9edbf56bd85c56ecbefe333d7e93
manishak <manishak@hcbspro.com>
2021-03-19 15:31:17 +0530

    Remove:unused and commented code

--- resources/views/pages/patient_detail/lab/drug-screen.blade.php
+++ resources/views/pages/patient_detail/lab/drug-screen.blade.php
@@ -2,105 +2,92 @@
     <div class="add-new-patient">
         <div class="icon"><img src="{{ asset('assets/img/icons/patient-img.svg') }}" class="img-fluid" /></div>
         @role('clinician')
-            <button type="button" onclick="onBroadCastOpen({{ $patient->id }})"
-                class="btn btn-outline-green w-600 d-table mr-auto ml-auto mt-3"
-                style="width: inherit;font-size: 18px;height: 36px;padding-left: 10px;padding-right: 10px;text-transform: uppercase;"
-            >Add New Request <span></span>
-            </button>
+            <button type="button" onclick="onBroadCastOpen('{{ $patient->id }}')" class="btn btn-outline-green w-600 d-table mr-auto ml-auto mt-3" style="width: inherit;font-size: 18px;height: 36px;padding-left: 10px;padding-right: 10px;text-transform: uppercase;">Add New Request<span></span></button>
         @endrole
         <div class="row">
             <div class="col-12 col-sm-1"></div>
             <div class="col-12 col-sm-10">
                 <table class="table table-bordered table-hover mt-4 drug-list-order">
                     <thead class="thead-light">
-                    <tr>
-                        <th scope="col">Sr. No.</th>
-                        <th scope="col">Test Type</th>
-                        <th scope="col">Due Date</th>
-                        <th scope="col">Performed Date</th>
-                        <th scope="col">Expiry Date</th>
-                        <th scope="col">Result</th>
-                        @role('clinician')
-                        <th width="11%">Reports</th>
-                        @endrole
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @php $number = 1; @endphp
-                    @foreach($drugLabReports as $drugLabReport)
-                        <tr class="drug-main-tr">
-                            <td scope="row">{{ $number }}</td>
-                            <td scope="row">{{ ($drugLabReport->labReportType) ? $drugLabReport->labReportType->name : ''}}</th>
-                            <td>{{ $drugLabReport->due_date }}</td>
-                            <td>{{ $drugLabReport->perform_date }}</td>
-                            <td>{{ $drugLabReport->expiry_date }}</td>
-                            <td>{{ $drugLabReport->result }}</td>
+                        <tr>
+                            <th scope="col">Sr. No.</th>
+                            <th scope="col">Test Type</th>
+                            <th scope="col">Due Date</th>
+                            <th scope="col">Performed Date</th>
+                            <th scope="col">Expiry Date</th>
+                            <th scope="col">Result</th>
                             @role('clinician')
-                                <td class='text-center'>
-                                    <!--<span
-                                        onclick="exploder('drug{{$number}}')" id="drug{{$number}}"
-                                        class="exploder"><i
-                                            class="las la-plus la-2x"></i></span>
-                                    <a href="javascript:void(0)" class="deleteLabResult" id="{{ $drugLabReport->id }}"><i
-                                            class="las la-trash la-2x pl-4"></i></a>-->
-                                    <input type="file" class="uploadLabResult" id="{{ $drugLabReport->labReportType->id }}" data-id="{{ $drugLabReport->labReportType->id }}" >
-                                    <!--<i class="las la-upload la-2x pl-4" ></i>-->
-                                    </input>
-                                </td>
+                                <th width="11%">Reports</th>
                             @endrole
                         </tr>
-                        <tr class="explode1 d-none">
-                            <td colspan="6">
-                                <!--<x-text-area name="note" placeholder="Enter note" value="{{$drugLabReport->note}}" class="note-area"/>-->
+                    </thead>
+                    <tbody>
+                        @php $number = 1; @endphp
+                        @if(count($drugLabReports) > 0)
+                            @foreach($drugLabReports as $drugLabReport)
                                 <x-hidden name="patient_lab_report_id" id="patient_lab_report_id" value="{{ $drugLabReport->id }}" />
-                            </td>
-                        </tr>
-                        @php $number++; @endphp
-                    @endforeach
-                    @role('clinician')
-                        <div class="alert alert-danger print-error-msg" style="display:none">
-                            <ul></ul>
-                        </div>
+                                <tr class="drug-main-tr">
+                                    <td scope="row">{{ $number }}</td>
+                                    <td scope="row">{{ ($drugLabReport->labReportType) ? $drugLabReport->labReportType->name : ''}}</th>
+                                    <td>{{ $drugLabReport->due_date }}</td>
+                                    <td>{{ $drugLabReport->perform_date }}</td>
+                                    <td>{{ $drugLabReport->expiry_date }}</td>
+                                    <td>{{ $drugLabReport->result }}</td>
+                                    @role('clinician')
+                                        <td class='text-center'>
+                                            <input type="file" class="uploadLabResult" id="{{ $drugLabReport->labReportType->id }}" data-id="{{ $drugLabReport->labReportType->id }}" ></input>
+                                        </td>
+                                    @endrole
+                                </tr>
+                                @php $number++; @endphp
+                            @endforeach
+                        @else
+                            <tr class="tb-main-tr no-record-tr"><td colspan="5" scope="row">No data available in table</td></tr>
+                        @endif
                         
-                        <tr>
-                            <form id="drugScreenForm">
-                                @csrf
-                                <th scope="row" class="drug-sequence">{{ ($drugLabReports) ? $drugLabReports->count() + 1 : '' }}</th>
-                                <td>
-                                    <select name="lab_report_type_id" class="form-control drug_lab_report_types">
-                                        <option value="">Select a test type</option>
-                                        @foreach($drugLabReportTypes as $drugLabReportType)
-                                            <option value="{{ $drugLabReportType->id }}">{{ $drugLabReportType->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('lab_report_type_id')
-                                    <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                    @enderror
-                                </td>
-                                <td><x-text name="lab_due_date" class="lab_due_date" /></td>
-                                <td><x-text name="lab_perform_date" class="lab_perform_date" /></td>
-                                <x-hidden name="patient_referral_id" class="patient_referral_id" value="{{ $paient_id }}" />
-                                <x-hidden name="lab_expiry_date" class="lab_expiry_date" />
-                                <td class="lab-expiry-date"></td>
-                                <td>
-                                    <select name="result" class="form-control">
-                                        <option value="">Select a result</option>
-                                        @foreach(config('select.labResult') as $key => $labResult)
-                                            <option value="{{ $key }}">{{ $labResult }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('result')
-                                    <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                    @enderror
-                                </td>
-                                <td></td>
-                            </form>
-                        </tr>
-                    @endrole
+                        @role('clinician')
+                            <tr>
+                                <div class="alert alert-danger print-error-msg" style="display:none">
+                                    <ul></ul>
+                                </div>
+                                <form id="drugScreenForm">
+                                    @csrf
+                                    <th scope="row" class="drug-sequence">{{ ($drugLabReports) ? $drugLabReports->count() + 1 : '' }}</th>
+                                    <td>
+                                        <select name="lab_report_type_id" class="form-control drug_lab_report_types">
+                                            <option value="">Select a test type</option>
+                                            @foreach($drugLabReportTypes as $drugLabReportType)
+                                                <option value="{{ $drugLabReportType->id }}">{{ $drugLabReportType->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('lab_report_type_id')
+                                        <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                        @enderror
+                                    </td>
+                                    <td><x-text name="lab_due_date" class="lab_due_date" /></td>
+                                    <td><x-text name="lab_perform_date" class="lab_perform_date" /></td>
+                                    <x-hidden name="patient_referral_id" class="patient_referral_id" value="{{ $paient_id }}" />
+                                    <x-hidden name="lab_expiry_date" class="lab_expiry_date" />
+                                    <td class="lab-expiry-date"></td>
+                                    <td>
+                                        <select name="result" class="form-control">
+                                            <option value="">Select a result</option>
+                                            @foreach(config('select.labResult') as $key => $labResult)
+                                                <option value="{{ $key }}">{{ $labResult }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('result')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </td>
+                                    <td></td>
+                                </form>
+                            </tr>
+                        @endrole
                     </tbody>
                 </table>
             </div>
--- resources/views/pages/patient_detail/lab/tb-screen.blade.php
+++ resources/views/pages/patient_detail/lab/tb-screen.blade.php
@@ -2,11 +2,7 @@
     <div class="add-new-patient">
         <div class="icon"><img src="{{ asset('assets/img/icons/patient-img.svg') }}" class="img-fluid" /></div>
         @role('clinician')
-            <button type="button" onclick="onBroadCastOpen({{ $patient->id }})"
-                    class="btn btn-outline-green w-600 d-table mr-auto ml-auto mt-3"
-                    style="width: inherit;font-size: 18px;height: 36px;padding-left: 10px;padding-right: 10px;text-transform: uppercase;"
-            >Add New Request <span></span>
-            </button>
+            <button type="button" onclick="onBroadCastOpen('{{ $patient->id }}')" class="btn btn-outline-green w-600 d-table mr-auto ml-auto mt-3" style="width: inherit;font-size: 18px;height: 36px;padding-left: 10px;padding-right: 10px;text-transform: uppercase;">Add New Request <span></span></button>
         @endrole
         <div class="row">
             <div class="col-12 col-sm-1"></div>
@@ -20,84 +16,77 @@
                             <th scope="col">Expiry Date</th>
                             <th scope="col">Result</th>
                             @role('clinician')
-                            <th width="11%">Reports</th>
+                                <th width="11%">Reports</th>
                             @endrole
                         </tr>
                     </thead>
                     <tbody>
                         @php $number = 1; @endphp
+                        @if(count($tbpatientLabReports) > 0)
+                            @foreach($tbpatientLabReports as $tbpatientLabReport)
+                                <x-hidden name="patient_lab_report_id" id="patient_lab_report_id" value="{{ $tbpatientLabReport->id }}" />
+                                <tr class="tb-main-tr">
+                                    <td scope="row">{{ $number }}</td>
+                                    <td scope="row">{{ ($tbpatientLabReport->labReportType) ? $tbpatientLabReport->labReportType->name : ''}}</td>
+                                    <td>{{ $tbpatientLabReport->due_date }}</td>
+                                    <td>{{ $tbpatientLabReport->expiry_date }}</td>
+                                    <td>{{ $tbpatientLabReport->result }}</td>
+                                    @role('clinician')
+                                        <td class='text-center'>
+                                            <input type="file" class="uploadLabResult" id="{{ $tbpatientLabReport->lab_report_type_id }}" data-id="{{ $tbpatientLabReport->patient_referral_id }}" >
+                                            </input>
+                                        </td>
+                                    @endrole
+                                </tr>
+                                
+                                @php $number++; @endphp
+                            @endforeach
+                        @else
+                            <tr class="tb-main-tr no-record-tr"><td colspan="5" scope="row">No data available in table</td></tr>
+                        @endif
 
-                        @foreach($tbpatientLabReports as $tbpatientLabReport)
-                            <tr class="tb-main-tr">
-                                <td scope="row">{{ $number }}</td>
-                                <td scope="row">{{ ($tbpatientLabReport->labReportType) ? $tbpatientLabReport->labReportType->name : ''}}</td>
-
-                                <td>{{ $tbpatientLabReport->due_date }}</td>
-                                <td>{{ $tbpatientLabReport->expiry_date }}</td>
-                                <td>{{ $tbpatientLabReport->result }}</td>
-                                @role('clinician')
-                                    <td class='text-center'>
-    <!--                                    <span
-                                            onclick="exploder('tb{{$number}}')" id="tb{{$number}}"
-                                            class="exploder"><i
-                                                class="las la-plus la-2x"></i></span>
-                                        <a href="javascript:void(0)" class="deleteLabResult" id="{{ $tbpatientLabReport->id }}" data-id="{{ $tbpatientLabReport->patient_referral_id }}"><i class="las la-trash la-2x pl-4" ></i></a>-->
-                                        <input type="file" class="uploadLabResult" id="{{ $tbpatientLabReport->lab_report_type_id }}" data-id="{{ $tbpatientLabReport->patient_referral_id }}" >
-                                        <!--<i class="las la-upload la-2x pl-4" ></i>-->
-                                        <!--<i class="las la-file-upload la-2x mr-2"></i>-->
-                                        </input>
-                                    </td>
-                                @endrole
-                            </tr>
-                            <tr class="explode1 d-none">
-                                <td colspan="6">
-                                    <!--<x-text-area name="note" placeholder="Enter note" value="{{$tbpatientLabReport->note}}" class="note-area"/>-->
-                                    <x-hidden name="patient_lab_report_id" id="patient_lab_report_id" value="{{ $tbpatientLabReport->id }}" />
-                                </td>
-                            </tr>
-                            @php $number++; @endphp
-                        @endforeach
                         @role('clinician')
-                        <tr>
-                            <div class="alert alert-danger print-error-msg" style="display:none">
-                                <ul></ul>
-                            </div>
-                            <form id="tbScreenForm">
-                                @csrf
-                                <td scope="row" class="tb-sequence">{{ (isset($tbpatientLabReports)) ? $tbpatientLabReports->count() + 1 : ''}}</td>
-                                <td>
-                                    <select name="lab_report_type_id" class="form-control tb_lab_report_types">
-                                        <option value="">Select a test type</option>
-                                        @foreach($tbLabReportTypes as $tbLabReportType)
-                                            <option value="{{ $tbLabReportType->id }}">{{ $tbLabReportType->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('lab_report_type_id')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </td>
-                                <td><x-text name="lab_due_date" class="lab_due_date" /></td>
-                                <x-hidden name="patient_referral_id" class="patient_referral_id" value="{{ $paient_id }}" />
-                                <x-hidden name="lab_expiry_date" class="lab_expiry_date" />
-                                <td class="lab-expiry-date"></td>
-                                <td>
-                                    <select name="result" class="result" class="form-control">
-                                        <option value="">Select a result</option>
-                                        @foreach(config('select.labResult') as $key => $labResult)
-                                            <option value="{{ $key }}">{{ $labResult }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('result')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </td>
-                                <td></td>
-                            </form>
-                        </tr>
+                            <tr>
+                                <div class="alert alert-danger print-error-msg" style="display:none">
+                                    <ul></ul>
+                                </div>
+                                <form id="tbScreenForm">
+                                    @csrf
+                                    <td scope="row" class="tb-sequence">{{ (isset($tbpatientLabReports)) ? $tbpatientLabReports->count() + 1 : ''}}</td>
+                                    <td>
+                                        <select name="lab_report_type_id" class="form-control tb_lab_report_types">
+                                            <option value="">Select a test type</option>
+                                            @foreach($tbLabReportTypes as $tbLabReportType)
+                                                <option value="{{ $tbLabReportType->id }}">{{ $tbLabReportType->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('lab_report_type_id')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </td>
+                                    <td><x-text name="lab_due_date" class="lab_due_date" /></td>
+                                    <x-hidden name="patient_referral_id" class="patient_referral_id" value="{{ $paient_id }}" />
+                                    <x-hidden name="lab_expiry_date" class="lab_expiry_date" />
+                                    
+                                    <td class="lab-expiry-date"></td>
+                                    <td>
+                                        <select name="result" class="result" class="form-control">
+                                            <option value="">Select a result</option>
+                                            @foreach(config('select.labResult') as $key => $labResult)
+                                                <option value="{{ $key }}">{{ $labResult }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('result')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </td>
+                                    <td></td>
+                                </form>
+                            </tr>
                         @endrole
                     </tbody>
                 </table>
@@ -105,9 +94,9 @@
             <div class="col-12 col-sm-1"></div>
         </div>
         @role('clinician')
-        <div class="d-flex pt-4 justify-content-center">
-            <button type="submit" class="btn btn-outline-green patient-detail-lab-report" name="Save">Save</button>
-        </div>
+            <div class="d-flex pt-4 justify-content-center">
+                <button type="submit" class="btn btn-outline-green patient-detail-lab-report" name="Save">Save</button>
+            </div>
         @endrole
     </div>
 </div>

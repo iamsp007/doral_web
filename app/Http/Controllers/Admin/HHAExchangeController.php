d06a5bd31d7ffe8fa694a462e9e4fe5fdecaccb7
manishak <manishak@hcbspro.com>
2021-03-30 16:42:15 +0530

    added:occupation detail

--- app/Http/Controllers/Admin/HHAExchangeController.php
+++ app/Http/Controllers/Admin/HHAExchangeController.php
@@ -20,25 +20,26 @@ class HHAExchangeController extends Controller
         // HHAExchange::dispatch();
         $searchPatientIds = $this->searchPatientDetails();
         $patientArray = $searchPatientIds['soapBody']['SearchPatientsResponse']['SearchPatientsResult']['Patients']['PatientID'];
-        // dd($patientArray);
-        // dump(count($patientArray));
-        //array_slice($patientArray, 0 , 1)
+       
+        dump(count($patientArray));
+
         // foreach ($patientArray as $patient_id) {
-        foreach (array_slice($patientArray, 0 , 1) as $patient_id) {
+        foreach (array_slice($patientArray, 0 , 500) as $patient_id) {
             $apiResponse = $this->getDemographicDetails($patient_id);
             $demographics = $apiResponse['soapBody']['GetPatientDemographicsResponse']['GetPatientDemographicsResult']['PatientInfo'];
-            // dd($demographics);
+           
             $user_id = self::storeUser($demographics);
 
             if ($user_id) {
-                self::storeDemographic($apiResponse, $user_id);
+                $type = config('constant.PatientType');
+                self::storeDemographic($demographics, $user_id, $type);
 
-                self::storeEmergencyContact($apiResponse, $user_id);
+                self::storeEmergencyContact($demographics, $user_id);
             }
         }
     }
 
-       /**
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -92,6 +93,17 @@ class HHAExchangeController extends Controller
 
     public static function storeUser($demographics)
     {      
+        $phone_number = setPhone($demographics['HomePhone']);
+        if ($phone_number == '') {
+            $status = '4';
+        } else {
+            $status = '0';
+
+            $userDuplicatePhone = User::whereNotNull('phone')->where('phone', $phone_number)->first();
+            if ($userDuplicatePhone) {
+                return;
+            }
+        }
 
         $doral_id = createDoralId();
         $password = str_replace(" ", "",$demographics['FirstName']) . '@' . $doral_id;
@@ -102,7 +114,7 @@ class HHAExchangeController extends Controller
         $user->password = setPassword($password);
         $user->phone = setPhone($demographics['HomePhone']);
         $user->phone_verified_at = now();
-
+        $user->status = $status;
         $user->gender = setGender($demographics['Gender']);
         
         $user->dob = dateFormat($demographics['BirthDate']);
@@ -112,7 +124,7 @@ class HHAExchangeController extends Controller
         return $user->id;
     }
 
-    public static function storeDemographic($demographics, $user_id)
+    public static function storeDemographic($demographics, $user_id, $type)
     {
         $doral_id = createDoralId();
 
@@ -121,7 +133,7 @@ class HHAExchangeController extends Controller
         $demographic->doral_id = $doral_id;
         $demographic->user_id = $user_id;
         $demographic->service_id = config('constant.MDOrder');
-        $demographic->patient_id = isset($demographics['PatientID']) ? $demographics['PatientID'] : '';
+        $demographic->patient_id = $demographics['PatientID'] ? $demographics['PatientID'] : '';
         $demographic->ssn = setSsn(isset($demographics['SSN']) ? $demographics['SSN'] : '');
 
         $accepted_services = [
@@ -154,31 +166,100 @@ class HHAExchangeController extends Controller
         ];
         $demographic->status = 'Active';
         $demographic->language = $language;
-        $demographic->type = config('constant.PatientType');
+        $demographic->type = $type;
 
         $demographic->save();
     }
 
     public static function storeEmergencyContact($demographics, $user_id)
     {
-        foreach ($demographics['EmergencyContacts']['EmergencyContact'] as $emergencyContact) {
-            if($emergencyContact['Name']) {
-                $relationship = '';
-                if ($emergencyContact['Relationship'] && $emergencyContact['Relationship']['Name']) {
-                    $relationship = $emergencyContact['Relationship']['Name'];
+        if (isset($demographics['EmergencyContacts']['EmergencyContact'])) {
+            foreach ($demographics['EmergencyContacts']['EmergencyContact'] as $emergencyContact) {
+                if($emergencyContact['Name']) {
+                    $relationship = '';
+                    if ($emergencyContact['Relationship'] && $emergencyContact['Relationship']['Name']) {
+                        $relationship = $emergencyContact['Relationship']['Name'];
+                    }
+                 
+                    $patientEmergencyContact = new PatientEmergencyContact();
+    
+                    $patientEmergencyContact->user_id = $user_id;
+                    $patientEmergencyContact->name = $emergencyContact['Name'];
+                    $patientEmergencyContact->relation = $relationship;
+                    $patientEmergencyContact->lives_with_patient = ($emergencyContact['LivesWithPatient']) ? $emergencyContact['LivesWithPatient'] : '';
+                    $patientEmergencyContact->have_keys = ($emergencyContact['HaveKeys']) ? $emergencyContact['HaveKeys'] : '';
+                    $patientEmergencyContact->phone1 = setPhone($emergencyContact['Phone1'] ? $emergencyContact['Phone1'] : '');
+                    $patientEmergencyContact->phone2 = setPhone($emergencyContact['Phone2'] ? $emergencyContact['Phone2'] : '');
+                    
+                    $patientEmergencyContact->address = ($emergencyContact['Address']) ? $emergencyContact['Address'] : '';
+                    $patientEmergencyContact->save();
                 }
-             
-                $patientEmergencyContact = new PatientEmergencyContact();
+            }
+        }
+       
+    }
 
-                $patientEmergencyContact->user_id = $user_id;
-                $patientEmergencyContact->name = $emergencyContact['Name'];
-                $patientEmergencyContact->relation = $relationship;
-                $patientEmergencyContact->lives_with_patient = ($emergencyContact['LivesWithPatient']) ? $emergencyContact['LivesWithPatient'] : '';
-                $patientEmergencyContact->phone1 = ($emergencyContact['Phone1']) ? $emergencyContact['Phone1'] : '';
-                $patientEmergencyContact->phone2 = ($emergencyContact['Phone2']) ? $emergencyContact['Phone2'] : '';
-                $patientEmergencyContact->address = ($emergencyContact['Address']) ? $emergencyContact['Address'] : '';
-                $patientEmergencyContact->save();
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function searchCaregivers()
+    {
+        $searchCaregiverIds = $this->searchCaregiverDetails();
+        $caregiverArray = $searchCaregiverIds['soapBody']['SearchCaregiversResponse']['SearchCaregiversResult']['Caregivers']['CaregiverID'];
+        dump(count($caregiverArray));
+
+        foreach (array_slice($caregiverArray, 0 , 1) as $cargiver_id) {
+
+            // foreach ($caregiverArray as $cargiver_id) {
+            /** Store patirnt demographic detail */
+            $userCaregiver = Demographic::where('caregiver_id' , $cargiver_id)->first();
+
+            if (! $userCaregiver) {
+                $getdemographicDetails = $this->getDemographicDetails($cargiver_id);
+                $demographics = $getdemographicDetails['soapBody']['GetCaregiverDemographicsResponse']['GetCaregiverDemographicsResult']['CaregiverInfo'];
+                dump($demographics);
+                $user_id = self::storeUser($demographics);
+
+                if ($user_id) {
+                    $type = config('constant.CaregiverType');
+                    self::storeDemographic($demographics, $user_id, $type);
+    
+                    self::storeEmergencyContact($demographics, $user_id);
+                }
             }
         }
     }
+
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function searchCaregiverDetails()
+    {
+        $data = '<?xml version="1.0" encoding="utf-8"?><SOAP-ENV:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Body><SearchCaregivers xmlns="https://www.hhaexchange.com/apis/hhaws.integration"><Authentication><AppName>HCHS257</AppName><AppSecret>99473456-2939-459c-a5e7-f2ab47a5db2f</AppSecret><AppKey>MQAwADcAMwAxADMALQAzADEAQwBDADIAQQA4ADUAOQA3AEEARgBDAEYAMwA1AEIARQA0ADQANQAyAEEANQBFADIAQgBDADEAOAA=</AppKey></Authentication><SearchFilters><Status>Active</Status><EmployeeType>Employee</EmployeeType></SearchFilters></SearchCaregivers></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+        //<FirstName>string</FirstName><LastName>string</LastName><PhoneNumber>string</PhoneNumber><CaregiverCode>string</CaregiverCode><EmployeeType>string</EmployeeType><SSN>string</SSN>Employee/Applicant
+
+        $method = 'POST';
+        return $this->curlCall($data, $method);
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getCaregiverDemographicDetails($cargiver_id)
+    {
+        $data = '<?xml version="1.0" encoding="utf-8"?><SOAP-ENV:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Body><GetCaregiverDemographics xmlns="https://www.hhaexchange.com/apis/hhaws.integration"><Authentication><AppName>HCHS257</AppName><AppSecret>99473456-2939-459c-a5e7-f2ab47a5db2f</AppSecret><AppKey>MQAwADcAMwAxADMALQAzADEAQwBDADIAQQA4ADUAOQA3AEEARgBDAEYAMwA1AEIARQA0ADQANQAyAEEANQBFADIAQgBDADEAOAA=</AppKey></Authentication><CaregiverInfo><ID>' . $cargiver_id . '</ID></CaregiverInfo></GetCaregiverDemographics></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+        $method = 'POST';
+        return $this->curlCall($data, $method);
+    }
+
 }

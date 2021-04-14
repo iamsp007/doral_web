215b87294829841869a87b0ce818c4cdcf074e8e
manishak <manishak@hcbspro.com>
2021-04-14 12:05:57 +0530

    Added:day wise cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,26 +35,15 @@ class Kernel extends ConsoleKernel
             $frequency = $apiRequest->schedule;
             $extra_schedule = $apiRequest->extra_schedule;
             $extra_schedule1 = explode(",", $extra_schedule);
-            log::info($extra_schedule);
-            // $scheduleData = '';
+            
             if ($frequency === '1') {
                 $schedule->command('ApiRequest:cron')->dailyAt('24:00')->timezone('America/New_York');
             } elseif ($frequency === '2') {
-                // $extra_schedule = strtoupper($extra_schedule);
-                foreach ($extra_schedule1 as $key => $value) {
-                    
-                    log::info($value);
-                    
+                foreach ($extra_schedule1 as $day) {
+                    $schedule->command('ApiRequest:cron')->weeklyOn($day,"24:00")->timezone('America/New_York');
                 }
-                // $scheduleData = 'days([Schedule::'.$value.', Schedule::'.$value.'],"24:00")';
-                // log::info($scheduleData);
-                // $scheduleData = 'twiceWeekly('.$extra_schedule.',"24:00")';
-            
-                // $scheduleData = "everyMinute";
-                $schedule->command('ApiRequest:cron')->twiceWeekly($extra_schedule,"24:00")->timezone('America/New_York');
             } else if ($frequency === '3') {
-                $scheduleData = 'twiceMonthly('.$extra_schedule.',"24:00")';
-                $schedule->command('ApiRequest:cron')->$scheduleData->timezone('America/New_York');
+                $schedule->command('ApiRequest:cron')->twiceMonthly($extra_schedule,"24:00")->timezone('America/New_York');
             } else if ($frequency === '4') {
                 $schedule->command('ApiRequest:cron')->quarterly()->timezone('America/New_York');;
             }

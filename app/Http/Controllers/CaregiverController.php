3b269c18f47aa07c8f235c967f7385ee05f0d8a8
manishahcbs <manishak@hcbspro.com>
2021-09-17 16:16:17 +0530

    Remove:unused code

--- app/Http/Controllers/CaregiverController.php
+++ app/Http/Controllers/CaregiverController.php
@@ -23,33 +23,7 @@ class CaregiverController extends Controller
 {
     public function index($serviceStatus = null,$initial = null)
     {
-        Log::info('Send notification for due report of patients starts');
-
-        $dateBetween['today'] =  date('Y-m-d');
-        
-        $date = Carbon::createFromFormat('Y-m-d', $dateBetween['today'])->subMonth();
-        $dateBetween['newDate'] = $date->format('Y-m-d');
-        
-        $patients = User::whereHas('patientLabReport',function ($q) use($dateBetween) {
-            $q->where('due_date',$dateBetween['newDate']);
-        })->get();
-       
-        $twomonthdate = Carbon::createFromFormat('Y-m-d', $dateBetween['today'])->addMonth(2);
-        $dateBetween['twomonthdate'] = $twomonthdate->format('Y-m-d');
-     
-        foreach ($patients as $patient) {
-           
-            $patientList = PatientLabReport::whereBetween('due_date',[$dateBetween['newDate'],$dateBetween['twomonthdate']])->where('user_id', $patient->id)->with('labReportType')->orderBy('due_date', 'ASC')->get();
-           
-            if (!$patientList->isEmpty()) {
-                if($patient->email) {
-                    Mail::to($patient->email)->send(new DueReportNotification($patientList, $patient->full_name));
-                } 
-            }
-        }
-
-        Log::info('Send notification for due report of patients end');
-        //return view('pages.patient_detail.new_patient', compact('serviceStatus', 'initial'));
+        return view('pages.patient_detail.new_patient', compact('serviceStatus', 'initial'));
     }
 
     public function dashboard()
